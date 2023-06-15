f2c949dfc03fe051b17aea00bef5deac98967b62
Jorge <jorgeavilacartes@gmail.com>
2023-06-15 18:01:48 +0200

    add function to get source and sink nodes for all paths

---
+++ panricci/utils/get_source_sink.py
@@ -0,0 +1,22 @@
+def get_sources_sinks(path_gfa):
+    sources = []
+    sinks   = []
+    with open(path_gfa, "r") as fp:
+        for line in fp.readlines():
+
+            # paths
+            nodes_path=[]
+            if line.startswith("P"):
+                _, seq_id, path, *_ = line.replace("\n","").split("\t")
+
+                nodes = path.split(",")
+                source = nodes[0]
+                sink = nodes[-1]
+
+                sourceid = source.replace("+","").replace("-","")
+                sinkid = sink.replace("+","").replace("-","")
+
+                sources.append(sourceid)
+                sinks.append(sinkid)
+    
+    return sources, sinks
