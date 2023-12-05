dc91eee906d0ec764d82bb956358d5f2d59fe084
Jorge Avila <jorgeavilacartes@gmail.com>
2023-12-05 14:11:42 +0100

    initialize weights and checkpoint in Ricci-Flow are now separated functions

--- panricci/ricci_flow.py
+++ panricci/ricci_flow.py
@@ -9,7 +9,12 @@ from pathlib import Path
 import logging
 class RicciFlow:
 
-    def __init__(self, G, distribution: Callable, dirsave_graphs: Optional[str] = None, overwrite: bool = True, tol=1e-11): 
+    def __init__(self, 
+                G, 
+                distribution: Callable, 
+                dirsave_graphs: Optional[str] = None, overwrite: bool = True, 
+                save_last: bool = True, save_intermediate_graphs: bool=False,
+                tol=1e-11): 
         # TODO: include threshold_curvature: float 
         # to stop ricci flow if all curvatures are at most at threshold_curvature from the average curvature
         # it means that is constant
@@ -17,29 +22,25 @@ class RicciFlow:
         self.G = G
         self.distribution_nodes = distribution # mapping from nodes to its distributions
         self.dirsave = dirsave_graphs
-        self.distance_by_edge = None
+        self.dirsave_graphs = dirsave_graphs
+        self.save_last = save_last
+        self.save_intermediate_graphs = save_intermediate_graphs
         self.tol = tol # tolerance of minimum curvature to stop Ricci-Flow when |K(u,v)| < tol for all (u,v) edge of G
  
         self._counter_iters = 0
 
         if overwrite is False and self.dirsave:
-            raise(f"""
-                    {str(self.dirsave)} already exists. To overwrite results set 'overwrite=True'
-                    or use another directory to save results.
-                """)
+            if self.dirsave.exists():
+                raise(f"{str(self.dirsave)} already exists. To overwrite results set 'overwrite=True' or use another directory to save results.")
 
         if self.dirsave:
             self.dirsave = Path(self.dirsave)
             self.dirsave.mkdir(exist_ok=True, parents=True)
 
-        # initialize curvature as 0 if it is not an attribute of the edges
-        n_edges=len(G.edges())
-        weight_init = 1/n_edges
-        for edge in G.edges():
-            self.G.edges[edge]["curvature"] = self.G.edges[edge].get("curvature",1)
-            self.G.edges[edge]["weight"] = self.G.edges[edge].get("weight",weight_init)
+        self.initialize_edges()
 
-    def run(self, iterations: int, save_last: bool = True, save_intermediate_graphs: bool=False, name=None):
+
+    def run(self, iterations: int, name=None):
         # TODO: add callbacks
         # save_last and save_intermediate_graphs should be a callback
         name = name if name else "graph"
@@ -61,16 +62,7 @@ class RicciFlow:
                 print(f"Stopping Ricci-Flow in iteration {it}, all curvatures below tol={self.tol}")
                 break
 
-            if save_intermediate_graphs:
-                name = name if name else "graph-iter"
-                # save graph with updated values
-                path_save = self.dirsave.joinpath(f"{name}-ricciflow-{it+1}.edgelist")
-                nx.write_edgelist(self.G, path_save, data=True)
-
-            if save_last:
-                # save last graph with attributes on its edges
-                path_save = self.dirsave.joinpath("{name}-ricciflow.edgelist")
-                nx.write_edgelist(self.G, path_save, data=True)
+            self.checkpoints(it, name)
 
         return self.G
             
@@ -140,3 +132,24 @@ class RicciFlow:
             if np.abs(data["curvature"]) > self.tol:
                 return False
         return True
+    
+    def checkpoints(self, it, name):
+        if self.save_intermediate_graphs:
+            name = name if name else "graph-iter"
+            # save graph with updated values
+            path_save = self.dirsave.joinpath(f"{name}-ricciflow-{it+1}.edgelist")
+            nx.write_edgelist(self.G, path_save, data=True)
+
+        if self.save_last:
+            # save last graph with attributes on its edges
+            path_save = self.dirsave.joinpath("{name}-ricciflow.edgelist")
+            nx.write_edgelist(self.G, path_save, data=True)
+
+    def initialize_edges(self,):
+        # initialize curvature as 0 if it is not an attribute of the edges
+        n_edges=len(self.G.edges())
+        weight_init = 1/n_edges
+
+        for edge in self.G.edges():
+            self.G.edges[edge]["curvature"] = self.G.edges[edge].get("curvature",1)
+            self.G.edges[edge]["weight"] = self.G.edges[edge].get("weight",weight_init)
